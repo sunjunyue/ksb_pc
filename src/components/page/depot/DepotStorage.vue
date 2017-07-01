b5c4913b8d424f4d3c11a8805aaf4b76a7489808
happyfather2009 <happyfather2009@163.com>
2017-07-01 16:48:56 +0800

    微调

--- src/components/page/depot/DepotStorage.vue
+++ src/components/page/depot/DepotStorage.vue
@@ -1,5 +1,7 @@
 <template>
+    <div class="win fr" id="win">
 
+    </div>
 </template>
 
 <script>
@@ -7,5 +9,14 @@
 </script>
 
 <style>
-
+    .win{
+        width: calc(100% - 328px);
+    }
+    .wm{
+        width: calc(100% - 100px)
+    }
+    .el-checkbox__label{
+        font-size:16px;
+    }
+    @import 'http://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
 </style>
