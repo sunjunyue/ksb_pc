bc05331c5110cc07b5733432c8d9bf590871e4d6
happyfather2009 <happyfather2009@163.com>
2017-07-01 17:55:27 +0800

    新增入库单页面

---
+++ src/components/page/depot/AddNewStorage.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="win fr" id="win">
+        <!--主题区域-->
+        <div class="wm">
+            <div class="wTit">| 新增入库单</div>
+
+
+            <div class="clear"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                url: 'http://192.168.31.243/ksbapi2/api/',
+                labelPosition: 'right',
+
+            }
+        }
+    }
+</script>
+
+<style>
+    .win{
+        width: calc(100% - 328px);
+    }
+    .wm{
+        width: calc(100% - 100px)
+    }
+    .el-checkbox__label{
+        font-size:16px;
+    }
+    .wm .el-form-item{
+        width:100%;
+    }
+    .el-form-item__content{
+        width:100%
+    }
+    .wm .el-input .el-input__inner{
+        width:100%;
+    }
+    @import 'http://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+</style>
