e822fc0375d02def18835d3926073cc9ca0761c9
happyfather2009 <happyfather2009@163.com>
2017-07-10 16:09:22 +0800

    微调页面错误

--- src/components/page/depot/DeMonthlyBalance.vue
+++ src/components/page/depot/DeMonthlyBalance.vue
@@ -11,6 +11,40 @@
 
 </script>
 
+<style scoped lang="less">
+    @import '../../../assets/css/ele_public.less';
+</style>
+
 <style>
+    @import '../../../assets/css/behind_cont.css';
+    @import 'http://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+</style>
+
+<style scoped>
+    .win {
+        width: calc(100% - 328px);
+    }
+
+    .wm {
+        width: calc(100% - 100px)
+    }
+
+    .el-checkbox__label {
+        font-size: 16px;
+    }
+
+    .wm .el-input .el-input__inner{
+        width:100%
+    }
+
+    .el-input__icon {
+        right: 4%!important;
+    }
+    .el-dialog {
+        background-color:#324057
+    }
+    .el-dialog__title{
+        color:#ffffff;
+    }
 
 </style>
