c41295d198d669f0fb56723f6d9d27a70c04e8e9
happyfather2009 <happyfather2009@163.com>
2017-07-10 15:49:39 +0800

    月度结余

---
+++ src/components/page/depot/DeMonthlyBalance.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="win fr" id="win">
+        <!--主题区域-->
+        <div class="wm">
+            <div class="wTit">| 月度结余</div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
