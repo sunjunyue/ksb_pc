c1df6ae9e151ff511f7ad72c0c61424240714f3d
sunjunyue <1501669484@qq.com>
2017-07-11 10:34:06 +0800

    微调页面错误

--- src/components/page/depot/DeMonthlyBalance.vue
+++ src/components/page/depot/DeMonthlyBalance.vue
@@ -97,6 +97,7 @@
                     options: [],
                     selectedOptions: [],
                 },
+                value4:''
             }
         },
         mounted: function () {
@@ -139,22 +140,22 @@
 </style>
 
 <style scoped>
-    .el-checkbox__label {
-        font-size: 16px;
-    }
+    /*.el-checkbox__label {*/
+        /*font-size: 16px;*/
+    /*}*/
 
-    .wm .el-input .el-input__inner{
-        width:100%
-    }
+    /*.wm .el-input .el-input__inner{*/
+        /*width:100%*/
+    /*}*/
 
-    .el-input__icon {
-        right: 4%!important;
-    }
-    .el-dialog {
-        background-color:#324057
-    }
-    .el-dialog__title{
-        color:#ffffff;
-    }
+    /*.el-input__icon {*/
+        /*right: 4%!important;*/
+    /*}*/
+    /*.el-dialog {*/
+        /*background-color:#324057*/
+    /*}*/
+    /*.el-dialog__title{*/
+        /*color:#ffffff;*/
+    /*}*/
 
 </style>
