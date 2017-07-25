383e81f6bf93a5793cec34f2d0c647cf02bf1e4a
sunjunyue <1501669484@qq.com>
2017-07-25 10:36:14 +0800

    调整

--- src/ksbconfig.js
+++ src/ksbconfig.js
@@ -2,7 +2,7 @@
  * Created by chenpeiqing on 2017/7/3.
  */
 
-const apiurl = 'http://192.168.31.242/ksbapi2/api/';
+const apiurl = 'http://192.168.31.71/ksbapi2/api/';
 const userphoto_token = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:1ae30GCkLjTeTumybxGuPS5F8Rk=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDA5ODgzMjJ9';
 const userphotebaseurl = 'http://osyuuevsn.bkt.clouddn.com/';
 
