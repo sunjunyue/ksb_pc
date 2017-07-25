70c760e42908fa130b53a0998226041060338b36
sunjunyue <1501669484@qq.com>
2017-07-25 12:57:06 +0800

    调整

--- src/components/page/developmenttask/TaskDetail.vue
+++ src/components/page/developmenttask/TaskDetail.vue
@@ -10,11 +10,17 @@
                              label-width="100px">
                         <el-row>
                             <!--form left-->
-                            <el-col :span="9">
+                            <el-col :span="8">
                                 <!--封面图-->
                                 <el-form-item label="封面图:">
-                                    <img :src="tasking_items[0].taskphotourl" width="100" height="100" style="border-radius:4px;">
+                                    <img :src="tasking_items[0].taskphotourl" width="200" height="200" style="border-radius:4px;">
                                 </el-form-item>
+                                <!--二维码-->
+                                <el-form-item label="二维码:">
+                                    <img :src="taskdetailurl[0].code" width="200" height="200" style="border-radius:4px;">
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="8" >
                                 <!--任务发起人-->
                                 <el-form-item label="发起人:">
                                     <el-input v-model="taskdetail.devtask_builder"></el-input>
@@ -25,7 +31,10 @@
                                 </el-form-item>
                                 <!--任务来源-->
                                 <el-form-item label="任务来源:">
-                                    <el-input v-model="taskdetail.devtask_source"></el-input>
+                                    <el-radio-group v-model="taskdetail.devtask_source">
+                                        <el-radio-button label="自定义任务"></el-radio-button>
+                                        <el-radio-button label="产品规划任务"></el-radio-button>
+                                    </el-radio-group>
                                 </el-form-item>
                                 <!--参考图-->
                                 <el-form-item label="参考图:">
@@ -33,66 +42,111 @@
                                         查看款式参考
                                     </el-button>
                                 </el-form-item>
-                            </el-col>
-                            <el-col :span="9" :push="3">
                                 <!--截止时间-->
                                 <el-form-item label="截止时间:">
-                                    <el-date-picker
-                                            v-model="taskdetail.devtask_deadline"
-                                            type="date"
-                                            placeholder="选择日期"
-                                            @change="dateChange"
-                                            style="width:100%">
-                                    </el-date-picker>
+                                    <el-input v-model="taskdetail.devtask_deadline"></el-input>
+
+                                    <!--<el-date-picker-->
+                                            <!--v-model="taskdetail.devtask_deadline"-->
+                                            <!--type="date"-->
+                                            <!--placeholder="选择日期"-->
+                                            <!--@change="dateChange"-->
+                                            <!--style="width:100%">-->
+                                    <!--</el-date-picker>-->
                                 </el-form-item>
-                                <!--二维码-->
-                                <el-form-item label="二维码:">
-                                    <img :src="taskdetailurl[0].code" width="100" height="100">
+                                <!--设计师-->
+                                <el-form-item label="设计师:">
+                                    <el-select v-model="taskdetail.devtask_designerid" placeholder="请选择" style="width:100%">
+                                        <el-option
+                                                v-for="item in designeroptions"
+                                                :key="item.value"
+                                                :label="item.label"
+                                                :value="item.value">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="8">
+                                <!--制版师-->
+                                <el-form-item label="制版师:">
+                                    <el-select v-model="taskdetail.devtask_patternmakerid" placeholder="请选择" style="width:100%">
+                                        <el-option
+                                                v-for="item in patternmakeroptions"
+                                                :key="item.value"
+                                                :label="item.label"
+                                                :value="item.value">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                                <!--裁剪师-->
+                                <el-form-item label="裁剪师:">
+                                    <el-select v-model="taskdetail.devtask_cutterid" placeholder="请选择" style="width:100%">
+                                        <el-option
+                                                v-for="item in cutteroptions"
+                                                :key="item.value"
+                                                :label="item.label"
+                                                :value="item.value">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                                <!--样衣工-->
+                                <el-form-item label="样衣工:">
+                                    <el-select v-model="taskdetail.devtask_yygid" placeholder="请选择" style="width:100%">
+                                        <el-option
+                                                v-for="item in yygoptions"
+                                                :key="item.value"
+                                                :label="item.label"
+                                                :value="item.value">
+                                        </el-option>
+                                    </el-select>
                                 </el-form-item>
                                 <!--具体要求-->
                                 <el-form-item label="具体要求：">
                                     <el-input type="textarea"
-                                          v-model="taskdetail.devtask_text"
-                                          :rows="7">
+                                              v-model="taskdetail.devtask_text"
+                                              :rows="7">
                                     </el-input>
                                 </el-form-item>
                             </el-col>
                         </el-row>
-                        <div class="wSub">
-                            <el-form-item>
-                                <el-button type="primary" @click="submitForm('formAccount')">提交更新</el-button>
-                                <el-button @click="resetForm('formAccount')">重置</el-button>
-                            </el-form-item>
-                        </div>
                     </el-form>
                 </el-tab-pane>
+                <!--任务进度-->
                 <el-tab-pane label="完成进度" name="second">
-                    <!--任务进度-->
-                        <el-steps :space="100" :active="1" :align-center="true">
-                            <el-step title="设计初稿" icon="edit"></el-step>
-                            <el-step title="初稿审核" icon="upload"></el-step>
-                            <el-step title="面辅料准备" icon="picture"></el-step>
-                            <el-step title="面辅料审核" icon="edit"></el-step>
-                            <el-step title="设计制版" icon="upload"></el-step>
-                            <el-step title="裁剪" icon="picture"></el-step>
-                            <el-step title="制作样衣" icon="edit"></el-step>
-                            <el-step title="定稿审核" icon="upload"></el-step>
-                        </el-steps>
+                    <el-steps :space="100" :active="1" :align-center="true">
+                        <el-step title="设计初稿" icon="edit"></el-step>
+                        <el-step title="初稿审核" icon="upload"></el-step>
+                        <el-step title="面辅料准备" icon="picture"></el-step>
+                        <el-step title="面辅料审核" icon="edit"></el-step>
+                        <el-step title="设计制版" icon="upload"></el-step>
+                        <el-step title="裁剪" icon="picture"></el-step>
+                        <el-step title="制作样衣" icon="edit"></el-step>
+                        <el-step title="定稿审核" icon="upload"></el-step>
+                    </el-steps>
                 </el-tab-pane>
             </el-tabs>
         </div>
     </div>
 </template>
+
 <script>
     export default{
         data(){
             return{
                 activeName2: 'first',
                 taskdetail:{
-
+                    devtask_builder:'',
+                    devtask_name:'',
+                    devtask_source:'自定义任务',
+                    devtask_deadline:'',
+                    devtask_designerid:'',
+                    devtask_patternmakerid:'',
+                    devtask_cutterid:'',
+                    devtask_yygid:'',
+                    devtask_text:''
                 },
                 taskdetailurl: [{
-                    code: 'http://osyuuevsn.bkt.clouddn.com/Fmf3iyMUtsMKz9MAH5HWtvtZ5MM7',
+                    code: 'http://192.168.31.71/ksbapi2/public/qrcodes/4f43e97f-ea7e-46f2-8aa8-0116dde1c069.png',
                 }],
                 radio: '1',
                 tasking_items: [{
