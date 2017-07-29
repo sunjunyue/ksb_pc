0e7a935431e4ed2a8fe3e050243495f89fe1102c
happyfather2009 <happyfather2009@163.com>
2017-07-29 09:04:47 +0800

    调整

--- src/ksbconfig.js
+++ src/ksbconfig.js
@@ -2,8 +2,8 @@
  * Created by chenpeiqing on 2017/7/3.
  */
 
-const apiurl = 'http://192.168.31.242/ksbapi2/api/';
-const userphoto_token = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:zD_IDROd70Ce8xDkxKxzBuHVMH0=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDEyNDc2MTF9';
+const apiurl = 'http://192.168.31.244/ksbapi2/api/';
+const userphoto_token = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:t8DPNAkmFt4yNCIIrBGLCmZ4smg=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDEzMzM0Mjd9';
 const userphotebaseurl = 'http://osyuuevsn.bkt.clouddn.com/';
 
 export default {
