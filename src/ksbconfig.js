21427094d40cc9236ac1626817e58539213951d5
sunjunyue <1501669484@qq.com>
2017-08-02 10:13:29 +0800

    调整

--- src/ksbconfig.js
+++ src/ksbconfig.js
@@ -4,7 +4,7 @@
 
 const apiurl = 'http://192.168.31.242/ksbapi2/api/';
 const apiurl2 = 'http://192.168.31.242/ksbapi2/public/qrcodes/';
-const userphoto_token = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:O-EOF1-yaw_GJ8rMpAWlxi5LjRk=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDE1OTQwODh9';
+const userphoto_token = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:a-OzlNGZNkwfccTpTr2Ny8egzz0=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDE2ODMxNTB9';
 const userphotebaseurl = 'http://osyuuevsn.bkt.clouddn.com/';
 
 export default {
