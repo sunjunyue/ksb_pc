af3620220e28c18944f576d8d2ce1df40a484955
sunjunyue <1501669484@qq.com>
2017-08-03 09:20:03 +0800

    调整

--- src/ksbconfig.js
+++ src/ksbconfig.js
@@ -2,9 +2,9 @@
  * Created by chenpeiqing on 2017/7/3.
  */
 
-const apiurl = 'http://192.168.31.242/ksbapi2/api/';
-const apiurl2 = 'http://192.168.31.242/ksbapi2/public/qrcodes/';
-const userphoto_token = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:a-OzlNGZNkwfccTpTr2Ny8egzz0=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDE2ODMxNTB9';
+const apiurl = 'http://192.168.31.243/ksbapi2/api/';
+const apiurl2 = 'http://192.168.31.243/ksbapi2/public/qrcodes/';
+const userphoto_token = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:WPK0BCyKI2doGLhxwltneEMCajI=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDE3NjYyNDZ9';
 const userphotebaseurl = 'http://osyuuevsn.bkt.clouddn.com/';
 
 export default {
